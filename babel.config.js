addcc4818ec6a7366539a83ba6e91f6ffceef0a1
kompanynia <kompany.nia@gmail.com>
2025-08-04 15:21:11 +0530

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,8 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      // Add these for production
+      '@babel/plugin-transform-export-namespace-from',
       'react-native-reanimated/plugin', // Must be last
     ],
   };
