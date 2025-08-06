87b7385cbe072b804549e233c0928486848e42e5
kompanynia <kompany.nia@gmail.com>
2025-08-06 18:02:19 +0530

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,9 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      // Change this line:
-      // 'react-native-reanimated/plugin',
-      // To this:
-      'react-native-worklets/plugin',
+      'react-native-reanimated/plugin',
     ],
   };
 };
